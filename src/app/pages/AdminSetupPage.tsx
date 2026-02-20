7117ed5538d210d9a02db058d87647f5811388d6
swampoff <n.moiseev@gmx.de>
2026-02-20 20:55:05 +0100

    fix: replace figma:asset in src/app/pages/AdminSetupPage.tsx to fix Vercel build

--- src/app/pages/AdminSetupPage.tsx
+++ src/app/pages/AdminSetupPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AdminSetup } from '../components/AdminSetup';
 import { FloatingParticles } from '../components/FloatingParticles';
 import { AnimatedWaves } from '../components/AnimatedWaves';
-import soulFmLogo from 'figma:asset/7dc3be36ef413fc4dd597274a640ba655b20ab3d.png';
+const soulFmLogo = '/favicon.ico'; // Automatically fixed figma asset import
 
 export function AdminSetupPage() {
   return (
