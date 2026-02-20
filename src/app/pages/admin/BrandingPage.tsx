bef121d2b84b5f7f1112c5497ce0b6e140e5b076
swampoff <n.moiseev@gmx.de>
2026-02-20 20:55:14 +0100

    fix: replace figma:asset in src/app/pages/admin/BrandingPage.tsx to fix Vercel build

--- src/app/pages/admin/BrandingPage.tsx
+++ src/app/pages/admin/BrandingPage.tsx
@@ -16,7 +16,7 @@ import {
 } from 'lucide-react';
 import { toast } from 'sonner';
 import { api } from '../../../lib/api';
-import soulFmLogo from 'figma:asset/7dc3be36ef413fc4dd597274a640ba655b20ab3d.png';
+const soulFmLogo = '/favicon.ico'; // Automatically fixed figma asset import
 
 interface BrandSettings {
   stationName: string;
